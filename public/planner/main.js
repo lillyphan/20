08c3eee3a29efaf9f74bc9dfab8577b6ee48db11
lillyphan <lphan24@kentdenver.org>
2022-12-20 07:47:30 -0700

    Add all initial references

---
+++ public/planner/main.js
@@ -0,0 +1,8 @@
+//initial references to the values of buttons
+let board = document.querySelector(".board");
+let farmButton = document.getElementById("new-farm");
+let clearFarmButton = document.getElementById("clear-farm");
+let eraseBtn = document.getElementById("erase-btn");
+let redBtn = document.getElementById("red");
+let blueBtn = document.getElementById("blue");
+let color;
